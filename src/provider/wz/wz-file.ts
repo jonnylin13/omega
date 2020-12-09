f13caadacee6055591a4f719288c145044f2b3eb
Jonny Lin <jonnylin13@gmail.com>
2020-12-08 16:59:59 -0800

    Implemented WZFile

--- src/provider/wz/wz-file.ts
+++ src/provider/wz/wz-file.ts
@@ -7,6 +7,8 @@ import { WZDirectoryEntry } from './wz-directory-entry';
 import * as fs from 'fs';
 import { GenericSeekableLittleEndianAccessor } from '../../util/data/input/generic-seekable-lea';
 import { GenericLittleEndianAccessor } from '../../util/data/input/generic-lea';
+import { WZTool } from './wz-tool';
+import { WZFileEntry } from './wz-file-entry';
 
 
 export class WZFile implements MapleDataProvider {
@@ -58,6 +60,48 @@ export class WZFile implements MapleDataProvider {
     }
 
     private parse_directory(dir: MapleDataDirectoryEntry): void {
-        
+        let entries = WZTool.read_value(this.lea);
+        for (let i = 0; i < entries; i++) {
+            let marker = this.lea.read_byte();
+            let name: string;
+            let size: number, checksum: number;
+            switch (marker) {
+                case 0x02:
+                    name = WZTool.read_decoded_string_at_offset_and_reset(this.slea, this.lea.read_int() + this.header_size + 1);
+                    size = WZTool.read_value(this.lea);
+                    checksum = WZTool.read_value(this.lea);
+                    this.lea.read_int();
+                    dir.files.push(new WZFileEntry(name, size, checksum, dir));
+                    break;
+                case 0x03:
+                case 0x04:
+                    name = WZTool.read_decoded_string(this.lea);
+                    size = WZTool.read_value(this.lea);
+                    checksum = WZTool.read_value(this.lea);
+                    this.lea.read_int();
+                    if (marker === 3) dir.subdirs.push(new WZDirectoryEntry(name, size, checksum, dir));
+                    else dir.files.push(new WZFileEntry(name, size, checksum, dir));
+                    break;
+                default:
+                    break;
+            }
+        }
+        for (let subdir of dir.subdirs) {
+            this.parse_directory(subdir);
+        }
+    }
+
+    // TODO: Implement
+    get_img_file(path: string): WZIMGFile {
+        return null;
+    }
+
+    get_data(path: string): MapleData {
+        let img_file = this.get_img_file(path);
+        if (img_file == null) {
+            return null;
+        }
+        let ret = img_file.root;
+        return ret;
     }
 }
