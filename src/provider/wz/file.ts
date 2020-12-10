34f40391decf0c5f13a0e2a828f5541263af98e3
Jonny Lin <jonnylin13@gmail.com>
2020-12-10 14:29:21 -0800

    Implemented WZXMLFile #45

--- src/provider/wz/file.ts
+++ src/provider/wz/file.ts
@@ -1,3 +1,6 @@
+import * as fs from 'fs';
+
+
 export class File {
     name: string;
     path: string;
@@ -5,10 +8,23 @@ export class File {
 
     constructor(path: string) {
         this.path = path;
+        this.name = path.includes('/') ? path.split('/').slice(-1)[0] : path;
     }
 
-    // TODO: Needs validation
     get_parent_file(): File {
-        return new File(this.path.split('/').slice(0, -1).join('/'));
+        let parent = new File(this.path.split('/').slice(0, -1).join('/'));
+        return parent;
     }
-}
+
+    list_files(): Array<File> {
+        let files = fs.readdirSync(this.path);
+        return files.map(file_name => new File(this.path + '/' + file_name));
+    }
+
+    is_directory(): boolean {
+        return fs.lstatSync(this.path).isDirectory();
+    }
+    
+    exists(): boolean {
+        return fs.existsSync(this.path);
+    }
--- test/provider/wz/wz-file.test.ts
+++ test/provider/wz/wz-file.test.ts
@@ -5,6 +5,6 @@ import { WZFile } from '../../../src/provider/wz/wz-file';
 
 describe('WZFile test', () => {
     it('should load a wz file', () => {
-        // let wz_file = new WZFile(new File('wz/Accessory/01010000.img.xml'), false);
+        // let wz_file = new WZFile(new File('wz/Character.wz/Accessory/01010000.img.xml'), false);
     });
 });
