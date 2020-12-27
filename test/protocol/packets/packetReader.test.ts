86249e669ba182028a4f752b291e80e9e2497524
Jonny Lin <jonnylin13@gmail.com>
2020-12-26 23:47:29 -0800

    Finished unit tests for PacketReader

--- test/protocol/packets/packetReader.test.ts
+++ test/protocol/packets/packetReader.test.ts
@@ -43,6 +43,49 @@ describe('protocol/packets/packetReader.ts', () => {
         expect(new PacketReader(double).readDouble()).to.equal(0.2);
     });
 
-    // TODO: Test string functions, read(), skip(), seek(), getRemainingPacket()
+    it('should read an ascii string', () => {
+        const string = Buffer.from([0x74, 0x68, 0x69, 0x73, 0x20, 0x69, 0x73, 0x20, 0x61, 0x20, 0x74, 0x65, 0x73, 0x74, 0x20, 0x73, 0x74, 0x72, 0x69, 0x6e, 0x67]);
+        expect(new PacketReader(string).readAsciiString(21)).to.equal('this is a test string');
+    });
+
+    it('should read a null terminated ascii string', () => {
+        const string = Buffer.from([0x74, 0x68, 0x69, 0x73, 0x20, 0x69, 0x73, 0x20, 0x61, 0x20, 0x74, 0x65, 0x73, 0x74, 0x20, 0x73, 0x74, 0x72, 0x69, 0x6e, 0x67, 0]);
+        expect(new PacketReader(string).readNullTerminatedAsciiString()).to.equal('this is a test string');
+    });
+
+    it('should read a maple ascii string', () => {
+        const string = Buffer.from([0x15, 0x0, 0x74, 0x68, 0x69, 0x73, 0x20, 0x69, 0x73, 0x20, 0x61, 0x20, 0x74, 0x65, 0x73, 0x74, 0x20, 0x73, 0x74, 0x72, 0x69, 0x6e, 0x67]);
+        expect(new PacketReader(string).readMapleAsciiString()).to.equal('this is a test string');
+    });
+
+    it('should read a position', () => {
+        const pos = Buffer.from([0x0, 0x1, 0x10, 0x00]);
+        const readPos = new PacketReader(pos).readPosition();
+        expect(readPos.x).to.equal(256);
+        expect(readPos.y).to.equal(16);
+    });
+
+    it('should read a number of bytes', () => {
+        const result = new PacketReader(Buffer.from([0x0, 0x01, 0x02, 0x03]));
+        expect(result.read(4).reduce((a, b) => a + b)).to.equal(6);
+    });
+
+    it('should skip a number of bytes', () => {
+        const result = new PacketReader(Buffer.from([0, 0x01, 0x02, 0x03]));
+        result.skip(2);
+        expect(result.readByte()).to.equal(0x02);
+    });
+
+    it('should set the offset to a certain number', () => {
+        const result = new PacketReader(Buffer.from([0, 0x01, 0x02, 0x03]));
+        result.seek(2);
+        expect(result.readByte()).to.equal(0x02);
+    });
+
+    it('should return the remaining buffer', () => {
+        const result = new PacketReader(Buffer.from([0xff, 0x01, 0x02, 0x03]));
+        result.readByte();
+        expect(result.getRemainingPacket().reduce((a, b) => a + b)).to.equal(6);
+    });
 
 });
